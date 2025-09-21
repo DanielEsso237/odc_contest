bd4cd806e36532476946a68e2f9cb0aeb532463a
DanielEsso <essodaniel55@gmail.com>
2025-09-21 14:15:52 +0100

    added event and trial selection pages, and displayed the publication page

--- contests/urls.py
+++ contests/urls.py
@@ -1,7 +1,10 @@
+# contests/urls.py
 from django.urls import path
 from . import views
 
 app_name = 'contests'
 urlpatterns = [
     path('events/', views.events_view, name='events'),
+    path('trials/<int:event_id>/', views.trials_view, name='trials'),
+    path('publications/<int:trial_id>/', views.publications_view, name='publications'),
 ]
